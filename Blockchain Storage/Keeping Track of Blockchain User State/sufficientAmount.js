baeda224cc72eea37ebd482e63a8ae3c6c8a3d5f
Luci4 <yamin.raad6109@gmail.com>
2024-12-04 14:41:45 +0600

    implemented sufficientAmount.js

---
+++ Blockchain Storage/Keeping Track of Blockchain User State/sufficientAmount.js
@@ -0,0 +1,28 @@
+class Transaction {
+  constructor(inputUTXOs, outputUTXOs) {
+    this.transaction = { inputUTXOs, outputUTXOs };
+  }
+  execute() {
+    const doubleSpent = this.transaction.inputUTXOs.findIndex(
+      (utxo) => utxo.spent === true
+    );
+    if (doubleSpent !== -1) {
+      throw new Error("Already spent");
+    }
+
+    const inputValue = this.transaction.inputUTXOs.reduce(
+      (sum, utxo) => sum + utxo.amount,
+      0
+    );
+    const outputValue = this.transaction.outputUTXOs.reduce(
+      (sum, utxo) => sum + utxo.amount,
+      0
+    );
+
+    if (inputValue < outputValue) {
+      throw new Error("Input value less than the output value");
+    }
+  }
+}
+
+module.exports = Transaction;
