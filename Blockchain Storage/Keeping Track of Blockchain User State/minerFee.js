bcda37dacf42d607cb122dfc279ae45d79b7ff1b
Luci4 <yamin.raad6109@gmail.com>
2024-12-04 15:09:06 +0600

    implemented minerFee.js

---
+++ Blockchain Storage/Keeping Track of Blockchain User State/minerFee.js
@@ -0,0 +1,38 @@
+class Transaction {
+  constructor(inputUTXOs, outputUTXOs) {
+    this.transaction = { inputUTXOs, outputUTXOs };
+  }
+  execute() {
+    const doubleSpent = this.transaction.inputUTXOs.findIndex(
+      (utxo) => utxo.spent === true
+    );
+    if (doubleSpent !== -1) {
+      throw new Error("Already spent");
+    }
+
+    const inputValue = this.transaction.inputUTXOs.reduce(
+      (sum, utxo) => sum + utxo.amount,
+      0
+    );
+    const outputValue = this.transaction.outputUTXOs.reduce(
+      (sum, utxo) => sum + utxo.amount,
+      0
+    );
+
+    if (inputValue < outputValue) {
+      throw new Error("Input value less than the output value");
+    }
+
+    const newInputUTXOs = this.transaction.inputUTXOs.map(
+      (utxo) => (utxo.spent = true)
+    );
+
+    this.transaction.inputUTXOs = newInputUTXOs;
+
+    const fee = inputValue - outputValue;
+
+    this.fee = fee;
+  }
+}
+
+module.exports = Transaction;
