96cfaa1450e13a26acb0b189f6a611b0d904aec0
Luci4 <yamin.raad6109@gmail.com>
2024-12-15 01:40:33 +0600

    charity.sol

---
+++ Smart Contract Basics/Address Interactions/charity.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Contract {
+    address public owner;
+    address public charityAddress;
+
+    constructor(address _charityAddress) {
+        owner = msg.sender;
+        charityAddress = _charityAddress;
+    }
+
+    receive() external payable {}
+
+    function tip() public payable {
+        (bool sent, ) = owner.call{value: msg.value}("");
+        require(sent);
+    }
+
+    function donate() public {
+        (bool sent, ) = charityAddress.call{value: address(this).balance}("");
+        require(sent);
+    }
+}
