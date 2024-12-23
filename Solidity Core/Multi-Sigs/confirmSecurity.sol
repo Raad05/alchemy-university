337a38e2b3b0cbdf79ab7f131834033df9273c5f
Luci4 <yamin.raad6109@gmail.com>
2024-12-23 17:50:32 +0600

    confirmSecurity.sol

---
+++ Solidity Core/Multi-Sigs/confirmSecurity.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract MultiSig {
+    address[] public owners;
+    uint public required;
+    uint public trxCount;
+
+    struct Transaction {
+        address destination;
+        uint amountInWei;
+        bool executed;
+    }
+
+    Transaction[] public transactions;
+    mapping(uint => Transaction) public transactionsById;
+
+    mapping(uint => mapping(address => bool)) public confirmations;
+
+    constructor(address[] memory _ownerAddresses, uint _requiredAmount) {
+        require(_ownerAddresses.length > 0, "No owner addresses sent");
+        require(_requiredAmount != 0, "Should be more than zero");
+        require(
+            _requiredAmount <= _ownerAddresses.length,
+            "Should be more than the total number of owner addresses"
+        );
+
+        owners = _ownerAddresses;
+        required = _requiredAmount;
+    }
+
+    function addTransaction(
+        address _destination,
+        uint _amount
+    ) internal returns (uint) {
+        Transaction memory transaction = Transaction(
+            _destination,
+            _amount,
+            false
+        );
+        transactions.push(transaction);
+        transactionsById[trxCount] = transaction;
+
+        uint trxId = trxCount;
+        trxCount++;
+
+        return trxId;
+    }
+
+    function confirmTransaction(uint _trxId) public {
+        require(isOwner(msg.sender), "Not among the owners");
+        confirmations[_trxId][msg.sender] = true;
+    }
+
+    function isOwner(address _address) public view returns (bool) {
+        for (uint i = 0; i < owners.length; i++) {
+            if (owners[i] == _address) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
