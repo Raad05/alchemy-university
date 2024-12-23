1e3d0702bb050bfa90813a1d3f979f2daf0539d0
Luci4 <yamin.raad6109@gmail.com>
2024-12-23 18:25:32 +0600

    super.sol

---
+++ Solidity Core/Inheritence/super.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "./Hero.sol";
+
+// TODO: create Mage/Warrior Heroes
+contract Mage is Hero(50) {
+    function attack(address _enemy) public override {
+        Enemy(_enemy).takeAttack(AttackTypes.Spell);
+        super.attack(_enemy);
+    }
+}
+
+contract Warrior is Hero(200) {
+    function attack(address _enemy) public override {
+        Enemy(_enemy).takeAttack(AttackTypes.Brawl);
+        super.attack(_enemy);
+    }
+}
