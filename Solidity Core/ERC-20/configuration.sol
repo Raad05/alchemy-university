07c100506b51c08aa11c525ebe0572dd4353db2e
Luci4 <yamin.raad6109@gmail.com>
2024-12-23 18:48:20 +0600

    configuration.sol

---
+++ Solidity Core/ERC-20/configuration.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+contract Token {
+    uint public totalSupply;
+    string public name = "Illuminani";
+    string public symbol = "ILN";
+    uint8 public decimals = 18;
+}
