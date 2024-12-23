b43aa844c23e086e6b616290ce44b3dcf451e816
Luci4 <yamin.raad6109@gmail.com>
2024-12-23 18:57:40 +0600

    transferEvent.sol

---
+++ Solidity Core/ERC-20/transferEvent.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+contract Token {
+    event Transfer(address sender, address recipient, uint amount);
+
+    uint public totalSupply;
+    string public name = "Illuminani";
+    string public symbol = "ILN";
+    uint8 public decimals = 18;
+
+    mapping(address => uint) public balances;
+
+    constructor() {
+        totalSupply = 1000 * (10 ** 18);
+        balances[msg.sender] = totalSupply;
+    }
+
+    function balanceOf(address _address) external view returns (uint) {
+        return balances[_address];
+    }
+
+    function transfer(address _address, uint _amount) public returns (bool) {
+        require(balances[msg.sender] >= _amount, "Not enough balance");
+
+        balances[_address] += _amount;
+        balances[msg.sender] -= _amount;
+
+        emit Transfer(msg.sender, _address, _amount);
+
+        return true;
+    }
+}
